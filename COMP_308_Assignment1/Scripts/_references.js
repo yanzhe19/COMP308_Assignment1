54588c78c8ac9c8397b5a5b8f98c0f1b1e642dce
yanzhe19 <yanzhe19@gmail.com>
2015-01-25 16:18:55 -0500

    Home page partial finished

--- COMP_308_Assignment1/Scripts/_references.js
+++ COMP_308_Assignment1/Scripts/_references.js
@@ -2,3 +2,7 @@
 /// <reference path="lib/foundation.min.js" />
 /// <reference path="lib/jquery.js" />
 /// <reference path="lib/app.js" />
+/// <reference path="../engine1/wowslider.mod.js" />
+/// <reference path="../engine1/wowslider.js" />
+/// <reference path="../engine1/script.js" />
+/// <reference path="../engine1/jquery.js" />
